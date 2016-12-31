b024a79d99aaca42870337dc5396b638f3c81a7a
xuehua <xuehua@gmail.com>
2016-12-30 20:28:31 -0800

    Update info.plist to allow only subdomains of stackoverflow.com to be accessed. Also added codes to use URLSessioin.shared.dataTask to get the contents of a URL.

--- DownloadWebContents/ViewController.swift
+++ DownloadWebContents/ViewController.swift
@@ -16,11 +16,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let myURL = URL(string:"http://stackoverflow.com") {
-            webView.loadRequest(URLRequest(url: myURL))
-            //webView.loadHTMLString("<H1>Hello</H1>", baseURL: nil)
-        }
+//        ///Use loadHTMLString to diplay HTML string in web view.
+//        webView.loadHTMLString("<H1>Hello</H1>", baseURL: nil)
         
+//        /// Use loadRequest to load a URL into webview
+//        if let myURL = URL(string:"https://stackoverflow.com") {
+//            webView.loadRequest(URLRequest(url: myURL))
+//        }
+        
+//        ///Use dataTask function to get contents for a URL
+//        if let myURL = URL(string:"https://stackoverflow.com") {
+//            let task = URLSession.shared.dataTask(with: myURL, completionHandler: {
+//                (data, respose, error) in
+//                
+//                if error != nil {
+//                    print(error as Any)
+//                } else {
+//                    if let unwrappedData = data {
+//                        let strData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+//                            as! String
+//                        print(strData)
+//                    }
+//                }
+//            })
+//            task.resume()
+//        }
+        
+        ///Use dataTask function for a URL request
+        if let myURL = URL(string:"http://www.stackoverflow.com") {
+            let myRequest = URLRequest(url: myURL)
+            let task = URLSession.shared.dataTask(with: myRequest, completionHandler: {
+                (data, response, error) in
+                
+                if error != nil {
+                    print(error as Any)
+                } else {
+                    if let unwrappedData = data {
+                        let strData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                            as! String
+                        print(strData)
+                    }
+                }
+            })
+            task.resume()
+        }
         
     }
 
